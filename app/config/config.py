9a4061efc1b371b58cea7f2bd62894c986ff71dd
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-03-07 10:28:27 +0530

    feat: create config file

---
+++ app/config/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class Config:
+    version = "0.1.0"
+    title = "Space to Storage Backend"
+
+    app_settings = {
+        'mongodb_url': os.getenv('MONGODB_URL'),
+        'db_name': os.getenv('DB_NAME'),
+        'db_username': os.getenv('DB_USERNAME'),
+        'db_password': os.getenv('DB_PASSWORD'),
+        'max_db_conn_count': int(os.getenv('MAX_DB_CONN_COUNT')),
+        'min_db_conn_count': int(os.getenv('MIN_DB_CONN_COUNT')),
+    }
